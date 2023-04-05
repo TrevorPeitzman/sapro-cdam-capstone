97b2e2d85d7b683bb9674d0764b959f1c8e6a68d
Ethan Sohn <m236000@usna.edu>
2023-04-05 09:25:01 -0400

    Added Admin.jsx pageAdded Admin.jsx page

--- sapro-cdam/src/pages/Admin.jsx
+++ sapro-cdam/src/pages/Admin.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function Admin() {
     return (
         <>
+            
             <h1>this is the Admin's page</h1>
             <h2>hopefully, eventually, behind an Admin privilege wall...</h2>
         </>
