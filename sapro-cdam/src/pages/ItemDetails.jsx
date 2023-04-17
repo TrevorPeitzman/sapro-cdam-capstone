3779fecced5fb1323cc61369ceb61b96e7a35815
TrevorPeitzman <trevor.peitzman@gmail.com>
2023-04-17 15:23:19 -0400

    start working with download blob (not working)

--- sapro-cdam/src/pages/ItemDetails.jsx
+++ sapro-cdam/src/pages/ItemDetails.jsx
@@ -20,6 +20,10 @@ const Item = styled(Paper)(({ theme }) => ({
 
 let flag = 0
 
+// export 
+
+//   // usage
+//   
 // This is the old FileUpload page!
 export function FileUploadPage() {
     const [filesToUpload, setFilesToUpload] = useState([]);
@@ -66,6 +70,32 @@ export function FileUploadPage() {
             }
         })
     }
+    
+    function downloadBlob(fileKey, filename) {
+        const result = Storage.get(fileKey, { download: true });
+        const url = URL.createObjectURL(result.Body);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = filename || 'download';
+        const clickHandler = () => {
+            setTimeout(() => {
+                URL.revokeObjectURL(url);
+                a.removeEventListener('click', clickHandler);
+            }, 150);
+        };
+        a.addEventListener('click', clickHandler, false);
+        a.click();
+        return a;
+    }
+
+    const downloadFile = (fileKey) => {
+        const result = Storage.get(fileKey, { download: true });
+        console.log(result)
+        // downloadBlob(result.Body, fileName);
+        const url = URL.createObjectURL(result.Body);
+        console.log(url)
+        return URL.createObjectURL(result.Body);
+    }
 
     const handleClose = (event, reason) => {
         if (reason === 'clickaway') {
@@ -111,7 +141,7 @@ export function FileUploadPage() {
                 <Typography variant="p" component="p" gutterBottom sx={{ textAlign: 'center' }} >
                     Reference: {itemDetails.reference}
                 </Typography>
-                
+
                 <Typography variant="p" component="p" gutterBottom sx={{ textAlign: 'center' }} >
                     Tier: {itemDetails.tier}
                 </Typography>
@@ -161,7 +191,7 @@ export function FileUploadPage() {
                                             <SpeedDialIcon />
                                         </ListItemIcon> */}
                                         {/* TODO: Need permissions in S3 to be set properly in order for this to work */}
-                                        <ListItemButton component="a" href={"https://sapro-cdam-document-store12606-dev.s3.amazonaws.com/public/" + file.key} target="_blank">
+                                        <ListItemButton component="a" href={downloadFile(file.key)} target="_blank">
                                             <ListItemText primary={file.key.split("/")[2]} />
                                         </ListItemButton>
                                     </ListItem>
