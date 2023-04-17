0082a4d9d5f96af98a679585a3767db70b5a9c3a
TrevorPeitzman <trevor.peitzman@gmail.com>
2023-04-17 15:33:10 -0400

    update admin page

--- sapro-cdam/src/pages/Admin.jsx
+++ sapro-cdam/src/pages/Admin.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
+import { Container, Typography, Link, Divider } from '@mui/material';
+
+import {
+    AccessRequestListCreateForm,
+    ChecklistCreateForm
+} from '../ui-components';
+
 function Admin() {
     return (
         <>
-            <h1>this is the Admin's page</h1>
-            <h2>hopefully, eventually, behind an Admin privilege wall...</h2>
+            <Container maxWidth="sm">
+                <Typography variant="h3" component="h2" gutterBottom sx={{ pt: 6, pb: 6, textAlign: 'center' }}>
+                    Admin Panel
+                </Typography>
+
+                <Typography variant="h5" component="h2" gutterBottom align='center'>
+                    Add a Command to the List
+                </Typography>
+
+                <ChecklistCreateForm />
+
+                <Divider variant="middle" />
+
+                {/* <Typography variant="h5" component="h2" gutterBottom align='center' sx={{ pt: 6, pb: 0, textAlign: 'center' }}>
+                    Request Access to the Admins list
+                </Typography> */}
+
+                {/* <AccessRequestListCreateForm /> */}
+
+            </Container>
         </>
     );
 }
